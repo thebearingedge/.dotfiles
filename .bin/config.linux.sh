cf5bbe09787d1da3e10e9f13b43d2d445b133d52
Tim Davis <contact@timdav.is>
2022-02-01 07:59:53 -0800

    use more vars in installer

--- .bin/config.linux.sh
+++ .bin/config.linux.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 git_dir="$HOME/.dotfiles.git"
+work_tree="$HOME"
 backup_dir="$HOME/.dotfiles.bak"
 
+https_url='https://github.com/thebearingedge/.dotfiles.git'
+ssh_url='git@github.com:thebearingedge/.dotfiles.git'
+
 dot() {
-  /usr/bin/git --git-dir="$git_dir" --work-tree="$HOME" "$@"
+  /usr/bin/git --git-dir="$git_dir" --work-tree="$work_tree" "$@"
 }
 
 config() {
@@ -15,24 +19,24 @@ config() {
   fi
 
   if [ ! -d "$git_dir" ]; then
-    echo 'cloning dotfiles...'
-    git clone --bare \
-      https://github.com/thebearingedge/.dotfiles.git \
-      "$git_dir"
+    echo 'cloning bare dotfiles repository ...'
+    git clone --bare "$https_url" "$git_dir"
+    dot config status.showUntrackedFiles no
+    dot remote set-url --push origin "$ssh_url"
   fi
 
-  if ! dot checkout > /dev/null 2>&1; then
-    echo "backing up displaced dot files to $backup_dir..."
-    files="$(dot checkout 2>&1 | grep -e '^[[:space:]]\.' | awk '{print $1}')"
+  files="$(dot checkout 2>&1 | grep -e '^[[:space:]]\.' | awk '{print $1}')"
+
+  if [ -n "$files" ]; then
+    echo "backing up displaced dotfiles to $backup_dir ..."
     for f in $files; do
-      echo "creating $backup_dir/$f"
-      mkdir -p "$backup_dir/$(dirname "$f")"
+      echo "$backup_dir/$f"
+      mkdir -p "$(dirname "$backup_dir/$f")"
       mv "$f" "$backup_dir/$f"
     done
+    unset f
+    dot checkout
   fi
-
-  dot checkout &&
-  dot config status.showUntrackedFiles no &&
 
   echo "dotfiles installed"
 }
